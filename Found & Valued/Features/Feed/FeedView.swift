a371ce53e3e9abe7bb319bac95f68516aba449b0
Zhi Yong Huang <huangdanny21@gmail.com>
2023-12-28 23:06:18 -0800

    Added feedview formatting

--- Found & Valued/Features/Feed/FeedView.swift
+++ Found & Valued/Features/Feed/FeedView.swift
@@ -10,28 +10,43 @@ import SwiftUI
 struct FeedView: View {
     @StateObject var viewModel = FeedViewModel()
 
+    let gridItems = [
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
+
     var body: some View {
         NavigationView {
-            VStack {
-                // Display fetched public feed items
-                List(viewModel.feedItems) { feedItem in
-                    // Display feed item details
-                    // Example:
-                    VStack(alignment: .leading) {
-                        Text(feedItem.itemTitle)
-                            .font(.headline)
-                        Text(feedItem.itemDescription)
-                            .font(.subheadline)
-                            .foregroundColor(.gray)
-                        // Add image display or other content here using the imageURL
+            ScrollView {
+                LazyVGrid(columns: gridItems, spacing: 10) {
+                    ForEach(viewModel.feedItems) { feedItem in
+                        // Display feed item content in each grid cell
+                        // Adjust the content as per your FeedItem structure
+                        VStack {
+                            
+                            if let imageURL = URL(string: feedItem.imageURL) {
+                                // Load and display the image using URLSession or your preferred library
+                                CachedImageView(url: imageURL, imageCache: ImageCache.shared)
+                            }
+                            
+                            Text(feedItem.itemTitle)
+                                .font(.headline)
+                            Text(feedItem.itemDescription)
+                                .font(.subheadline)
+                                .foregroundColor(.gray)
+                            // Add image display or other content here using the imageURL
+                        }
+                        .padding()
+                        .background(Color.gray.opacity(0.1))
+                        .cornerRadius(8)
                     }
                 }
+                .padding(8)
             }
             .navigationTitle("Public Feeds")
             .onAppear {
-                viewModel.fetchPublicFeeds() // Fetch public feeds when the view appears
+                viewModel.fetchPublicFeeds()
             }
         }
     }
 }
-
