6abf77ac369ce4395478141ca821ffc909abfa45
Zhi Yong Huang <huangdanny21@gmail.com>
2023-12-28 23:10:58 -0800

    Added Sections for feeds

--- Found & Valued/Features/Feed/FeedView.swift
+++ Found & Valued/Features/Feed/FeedView.swift
@@ -10,39 +10,30 @@ import SwiftUI
 struct FeedView: View {
     @StateObject var viewModel = FeedViewModel()
 
-    let gridItems = [
-        GridItem(.flexible()),
-        GridItem(.flexible())
-    ]
-
     var body: some View {
         NavigationView {
-            ScrollView {
-                LazyVGrid(columns: gridItems, spacing: 10) {
-                    ForEach(viewModel.feedItems) { feedItem in
-                        // Display feed item content in each grid cell
-                        // Adjust the content as per your FeedItem structure
-                        VStack {
-                            
-                            if let imageURL = URL(string: feedItem.imageURL) {
-                                // Load and display the image using URLSession or your preferred library
-                                CachedImageView(url: imageURL, imageCache: ImageCache.shared)
-                            }
-                            
-                            Text(feedItem.itemTitle)
-                                .font(.headline)
-                            Text(feedItem.itemDescription)
-                                .font(.subheadline)
-                                .foregroundColor(.gray)
-                            // Add image display or other content here using the imageURL
+            List(viewModel.feedItems) { feedItem in
+                Section {
+                    // Display the feed item content in a single section
+                    VStack {
+                        if let imageURL = URL(string: feedItem.imageURL) {
+                            // Load and display the image using URLSession or your preferred library
+                            CachedImageView(url: imageURL, imageCache: ImageCache.shared)
                         }
-                        .padding()
-                        .background(Color.gray.opacity(0.1))
-                        .cornerRadius(8)
+                        Text(feedItem.itemTitle)
+                            .font(.headline)
+                        Text(feedItem.itemDescription)
+                            .font(.subheadline)
+                            .foregroundColor(.gray)
+                        // Add image display or other content here using the imageURL
                     }
+                    .padding()
+                    .background(Color.gray.opacity(0.1))
+                    .cornerRadius(8)
+                    .frame(maxWidth: .infinity)
                 }
-                .padding(8)
             }
+            .listStyle(GroupedListStyle())
             .navigationTitle("Public Feeds")
             .onAppear {
                 viewModel.fetchPublicFeeds()
@@ -50,3 +41,4 @@ struct FeedView: View {
         }
     }
 }
+
